e49638068a6891342c1a5ff2046bbb407177e4ed
Sydney Selch <sydney_selch@Sydneys-MacBook-Pro-4.local>
2015-03-20 15:57:40 -0400

    Creative cloud icons added and javascript added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 var $hamBtn = $('.ham-btn');
 var $panel = $('.panel');
 
+var $ai = $('.square-ai');
+var $ps = $('.square-ps');
+var $id = $('.square-id');
+var $ai2 = $('.ai2-icon');
+var $ps2 = $('.ps2-icon');
+var $id2 = $('.id2-icon');
+var $wand = $('.wand');
+
 
 $hamBtn.on('click', function () {
     $panel.toggleClass('js-panel-open');
 });
+
+
+$ai.on('click', function () {
+    $ai.toggleClass('js-square-ai');
+});
+
+
+$ps.on('click', function () {
+    $ps.removeClass('js-square-ps');
+    $ps.toggleClass('js-wand'); 
+});
+
+$wand.on('webkitAnimationEnd animationend', function (){
+    $ps.addClass('js-square-ps');
+});
+
+
+
+
+$id.on('click', function () {
+    $id.toggleClass('js-square-id');
+});
+
+
+
+
